3682efc1f2da50e0a7f565c64c8d4212252dabad
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-08-31 23:01:54 +0300

    Create bank task

---
+++ src/main/java/nevg/banktask/BankTaskApplication.java
@@ -0,0 +1,13 @@
+package nevg.banktask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nevg/banktask/BankTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package nevg.banktask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
