a0341632b4fe9d689da8e9800ebf8f1a033c2551
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-09-01 01:01:57 +0300

    Add user controller

---
+++ src/main/java/nevg/banktask/Controller/UserController.java
@@ -0,0 +1,30 @@
+package nevg.banktask.Controller;
+
+import jakarta.validation.Valid;
+import nevg.banktask.DTO.RegisterUserDTO;
+import nevg.banktask.Service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/account/")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("register")
+    ResponseEntity<?> getRegisterInfo(@Valid @RequestBody RegisterUserDTO registerUserDTO) {
+        System.out.println();
+        long id = 2L;
+
+        return ResponseEntity
+                .created(URI.create("/api/users/" + id))
+                .build();
+    }
+}
---
+++ src/main/java/nevg/banktask/DTO/RegisterUserDTO.java
@@ -0,0 +1,26 @@
+package nevg.banktask.DTO;
+
+public class RegisterUserDTO {
+
+    private String email;
+    private String password;
+
+    public RegisterUserDTO() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
