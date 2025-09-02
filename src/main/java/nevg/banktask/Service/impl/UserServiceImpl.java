0c4c81fa02c0e3a396885abaf372c3ff8a60687a
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-09-03 01:47:43 +0300

    Add spring security

---
+++ src/main/java/nevg/banktask/Service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package nevg.banktask.Service.impl;
+
+import nevg.banktask.Service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
